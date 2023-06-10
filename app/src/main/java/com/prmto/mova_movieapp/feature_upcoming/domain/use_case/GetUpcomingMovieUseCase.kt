2a2146ca3a73d5a9cde61324347cf8a93d862096
Tolga Pirim <tolgapirim25@gmail.com>
2023-06-10 19:04:54 +0300

    Compare a given release date with the current date to get the upcoming movies. Added ripple effect to the info and the remindMe buttons

--- app/src/main/java/com/prmto/mova_movieapp/feature_upcoming/domain/use_case/GetUpcomingMovieUseCase.kt
+++ app/src/main/java/com/prmto/mova_movieapp/feature_upcoming/domain/use_case/GetUpcomingMovieUseCase.kt
@@ -1,6 +1,7 @@
 package com.prmto.mova_movieapp.feature_upcoming.domain.use_case
 
 import androidx.paging.PagingData
+import androidx.paging.filter
 import androidx.paging.map
 import com.prmto.mova_movieapp.core.data.dto.Genre
 import com.prmto.mova_movieapp.core.domain.models.Movie
@@ -9,13 +10,14 @@ import com.prmto.mova_movieapp.core.presentation.util.HandleUtils
 import com.prmto.mova_movieapp.feature_upcoming.domain.repository.UpComingRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import java.util.Calendar
 import javax.inject.Inject
 
 class GetUpcomingMovieUseCase @Inject constructor(
     private val repository: UpComingRepository,
     private val movieGenreListUseCase: GetMovieGenreListUseCase
 ) {
-
+    private val calendar = Calendar.getInstance()
     operator fun invoke(language: String): Flow<PagingData<Movie>> {
         val languageLower = language.lowercase()
         return combine(
@@ -32,9 +34,27 @@ class GetUpcomingMovieUseCase @Inject constructor(
                     ),
                     voteCountByString = HandleUtils.convertingVoteCountToString(movie.voteCount)
                 )
-
+            }.filter { movie ->
+                movie.releaseDate?.let { releaseDate ->
+                    isAfterReleaseDate(releaseDate)
+                } ?: false
             }
         }
-
     }
+
+
+    private fun isAfterReleaseDate(releaseDate: String): Boolean {
+        val releaseYear = releaseDate.split("-")[0].toInt()
+        val releaseMonth = releaseDate.split("-")[1].toInt() - 1
+        val releaseDay = releaseDate.split("-")[2].toInt()
+
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return releaseYear >= currentYear &&
+                releaseMonth >= currentMonth &&
+                releaseDay >= currentDay
+    }
+
 }
