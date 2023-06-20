97d6ac886f4eb9d0fd2af7b9a7ea7d9f02d44fe9
Tolga Pirim <tolgapirim25@gmail.com>
2023-06-20 11:36:56 +0300

    Refactor that updating collect flows with extension functions in feat/splash

--- app/src/main/java/com/prmto/mova_movieapp/feature_splash/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/prmto/mova_movieapp/feature_splash/presentation/splash/SplashFragment.kt
@@ -8,15 +8,11 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.prmto.mova_movieapp.R
+import com.prmto.mova_movieapp.core.presentation.util.collectFlow
 import com.prmto.mova_movieapp.feature_splash.presentation.splash.event.SplashEvent
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
@@ -35,29 +31,25 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    viewModel.eventFlow.collectLatest { event ->
-                        when (event) {
-                            is SplashEvent.NavigateTo -> {
-                                findNavController().navigate(event.directions)
-                            }
-                            is SplashEvent.UpdateAppLanguage -> {
-                                AppCompatDelegate.setApplicationLocales(
-                                    LocaleListCompat.forLanguageTags(
-                                        event.language
-                                    )
-                                )
-                            }
-                            is SplashEvent.UpdateUiMode -> {
-                                if (event.uiMode == AppCompatDelegate.MODE_NIGHT_YES) {
-                                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                                } else {
-                                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                                }
-                            }
-                        }
+        collectFlow(viewModel.eventFlow) { event ->
+            when (event) {
+                is SplashEvent.NavigateTo -> {
+                    findNavController().navigate(event.directions)
+                }
+
+                is SplashEvent.UpdateAppLanguage -> {
+                    AppCompatDelegate.setApplicationLocales(
+                        LocaleListCompat.forLanguageTags(
+                            event.language
+                        )
+                    )
+                }
+
+                is SplashEvent.UpdateUiMode -> {
+                    if (event.uiMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                     }
                 }
             }
