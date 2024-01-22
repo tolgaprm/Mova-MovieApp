674ec3bb9ab2dabfeb3343fbb5bf41b99fd2e672
Tolga Pirim <tolgapirim25@gmail.com>
2024-01-22 17:27:57 +0300

    Mova - 103 Show informative message in MyListScreen #22
    - Add a error_screen in MyList if user doesn't have any items in  favorite and watch list

--- my-list/my-list-ui/src/main/java/com/prmto/my_list_ui/ListState.kt
+++ my-list/my-list-ui/src/main/java/com/prmto/my_list_ui/ListState.kt
@@ -1,5 +1,6 @@
 package com.prmto.my_list_ui
 
+import androidx.annotation.StringRes
 import com.prmto.core_domain.models.movie.Movie
 import com.prmto.core_domain.models.tv.TvSeries
 
@@ -8,7 +9,9 @@ data class ListState(
     val chipType: ChipType = ChipType.MOVIE,
     val isLoading: Boolean = false,
     val movieList: List<Movie> = emptyList(),
-    val tvSeriesList: List<TvSeries> = emptyList()
+    val tvSeriesList: List<TvSeries> = emptyList(),
+    val hasAnyMovieInList: Boolean = false,
+    @StringRes val errorMessage: Int = R.string.not_tv_in_your_list
 )
 
 enum class ListTab {
